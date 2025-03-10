e3d79a93eb4a4a162f44ae32c5fa766f88414853
shipopala <shishkin.pa@phystech.edu>
2025-03-10 13:35:02 +0300

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -21,6 +21,7 @@ void LavalNozzle::solveNavierStokes(std::vector<double>& pressure, std::vector<d
     int num_points = pressure.size();
     double dx = L / (num_points - 1);
     double A_throat = calculateArea(D_throat);
+    double A_max = 10 * A_throat; // Максимальная площадь сечения
 
     // Начальные условия
     pressure[0] = P0;
@@ -32,22 +33,29 @@ void LavalNozzle::solveNavierStokes(std::vector<double>& pressure, std::vector<d
         double D = nozzleProfile(x);
         double A = calculateArea(D);
 
-        // Изоэнтропические соотношения (исправлено для температуры)
+        // Ограничение площади сечения
+        if (A > A_max) A = A_max;
+
+        // Изоэнтропические соотношения
         pressure[i] = P0 * pow(A_throat / A, (2 * gamma) / (gamma - 1));
-        temperature[i] = T0 * pow(A_throat / A, (gamma - 1) / gamma); // Исправлено
+        temperature[i] = T0 * pow(A_throat / A, (gamma - 1) / gamma);
         velocity[i] = (m_dot * R * temperature[i]) / (pressure[i] * A);
 
-        // Поправка на вязкость и теплопроводность
+        // Поправка на вязкость
         double dp_dx = -mu * velocity[i-1] / (D * D);
         pressure[i] += dp_dx * dx;
+        if (pressure[i] < 0) pressure[i] = 0; // Ограничение давления
 
+        // Поправка на теплопроводность
         double dT_dx = -k * (temperature[i-1] - T0) / (D * D);
         temperature[i] += dT_dx * dx;
     }
 
     // Корректировка выходного давления
     pressure[num_points - 1] = Pe;
+    if (pressure[num_points - 1] < 0) pressure[num_points - 1] = 0; // Ограничение давления
 }
+
 // Расчет профиля сопла
 std::vector<double> LavalNozzle::calculateNozzleProfile(int num_points) {
     std::vector<double> profile(num_points);
